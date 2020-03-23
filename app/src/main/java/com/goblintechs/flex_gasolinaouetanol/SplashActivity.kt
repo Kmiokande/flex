385cfef71e828e6e9b85b96cce6e7667136938b5
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-03-23 15:51:34 -0300

    Adicionando tela splash

---
+++ app/src/main/java/com/goblintechs/flex_gasolinaouetanol/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.goblintechs.flex_gasolinaouetanol
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity() {
+    private val tempoSplash = 2000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_activity)
+
+        Handler().postDelayed(Runnable {
+            mainScreen()
+        }, tempoSplash.toLong())
+    }
+
+    fun mainScreen() {
+        val i = Intent(this@SplashActivity, MainActivity::class.java)
+        startActivity(i)
+        finish()
+    }
+}
