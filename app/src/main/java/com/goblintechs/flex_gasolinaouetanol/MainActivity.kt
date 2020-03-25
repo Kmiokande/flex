85ac67c5cba7f45741bb545fb468c85d1298f99a
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-03-24 22:03:32 -0300

    Adicionando menu com a opção sair

--- app/src/main/java/com/goblintechs/flex_gasolinaouetanol/MainActivity.kt
+++ app/src/main/java/com/goblintechs/flex_gasolinaouetanol/MainActivity.kt
@@ -2,6 +2,8 @@ package com.goblintechs.flex_gasolinaouetanol
 
 import android.app.AlertDialog
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
@@ -17,6 +19,23 @@ class MainActivity : AppCompatActivity() {
         btCalculate.setOnClickListener {
             calculate()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle presses on the action bar menu items
+        when (item.itemId) {
+            R.id.action_exit -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun calculate() {
