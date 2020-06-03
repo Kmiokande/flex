68059661bf46d93662614d0103a48c4f28ca0a47
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-06-03 15:59:09 -0300

    Atualizando projeto

--- app/src/main/java/com/goblintechs/flex_gasolinaouetanol/activities/CarsActivity.kt
+++ app/src/main/java/com/goblintechs/flex_gasolinaouetanol/activities/CarsActivity.kt
@@ -1,20 +1,26 @@
 package com.goblintechs.flex_gasolinaouetanol.activities
 
-import android.app.Dialog
-import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.widget.Toast
+import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.goblintechs.flex_gasolinaouetanol.R
 import com.goblintechs.flex_gasolinaouetanol.adapters.ListCarAdapter
 import com.goblintechs.flex_gasolinaouetanol.classes.ExampleItem
+import com.goblintechs.flex_gasolinaouetanol.models.Car
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.cars_activity.*
+import kotlinx.android.synthetic.main.dialog_car.*
 import kotlinx.android.synthetic.main.dialog_car.view.*
+import kotlinx.android.synthetic.main.dialog_car.view.etNameCar
+import kotlinx.android.synthetic.main.dialog_car.view.tlNameCar
 
 class CarsActivity : AppCompatActivity() {
+    private val referencia: DatabaseReference = FirebaseDatabase.getInstance().getReference()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +39,27 @@ class CarsActivity : AppCompatActivity() {
             val mAlertDialog = mBuilder.show()
             mDialogView.btCarCancel.setOnClickListener {
                 mAlertDialog.dismiss()
+            }
+            mDialogView.btCarSave.setOnClickListener {
+                valFields(mDialogView)
+
+                if (mDialogView.etNameCar.text!!.isNotEmpty() &&
+                    mDialogView.etGasCarConsum.text!!.isNotEmpty() &&
+                    mDialogView.etEthaCarConsum.text!!.isNotEmpty()) {
+
+                    val nameCar = mDialogView.etNameCar.text.toString()
+                    val gasConsum = mDialogView.etGasCarConsum.text.toString()
+                    val ethaConsum = mDialogView.etEthaCarConsum.text.toString()
+
+                    val car = Car(nameCar, gasConsum, ethaConsum)
+                    val usuarios: DatabaseReference = referencia.child("usuarios")
+                    val user = FirebaseAuth.getInstance().currentUser
+                    val uid = user?.uid
+                    usuarios.child(uid.toString()).child("carros").push().setValue(car)
+
+                    mAlertDialog.dismiss()
+                }
+
             }
         }
     }
@@ -124,6 +151,32 @@ class CarsActivity : AppCompatActivity() {
         )
 
         return list
+    }
+
+    private fun valFields(dialogView: View) {
+        if (dialogView.etNameCar.text!!.isEmpty()) {
+            dialogView.tlNameCar.isErrorEnabled = true
+            dialogView.tlNameCar.error = getString(R.string.name_car_error)
+        }
+        else {
+            dialogView.tlNameCar.isErrorEnabled = false
+        }
+
+        if (dialogView.etGasCarConsum.text.toString().isEmpty()) {
+            dialogView.tlGasCarConsum.isErrorEnabled = true
+            dialogView.tlGasCarConsum.error = getString(R.string.gasoline_consum_error)
+        }
+        else {
+            dialogView.tlGasCarConsum.isErrorEnabled = false
+        }
+
+        if (dialogView.etEthaCarConsum.text!!.isEmpty()) {
+            dialogView. tlEthaCarConsum.isErrorEnabled = true
+            dialogView. tlEthaCarConsum.error = getString(R.string.ethanol_consum_error)
+        }
+        else {
+            dialogView.tlEthaCarConsum.isErrorEnabled = false
+        }
     }
 
     override fun onBackPressed() {
---
+++ app/src/main/java/com/goblintechs/flex_gasolinaouetanol/models/Car.kt
@@ -0,0 +1,5 @@
+package com.goblintechs.flex_gasolinaouetanol.models
+
+class Car(var nome: String, var gasConsum: String, var ethaConsum: String) {
+
+}
