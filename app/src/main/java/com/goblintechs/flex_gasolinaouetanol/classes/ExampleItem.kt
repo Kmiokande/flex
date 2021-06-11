cef2089d526f4f58adb547f689e4bc203029a8c4
Kmiokande <brunnodesouzabezerra@gmail.com>
2021-06-11 11:49:53 -0300

    Criando projeto react-native

--- app/src/main/java/com/goblintechs/flex_gasolinaouetanol/classes/ExampleItem.kt
+++
@@ -1,5 +0,0 @@
-package com.goblintechs.flex_gasolinaouetanol.classes
-
-data class ExampleItem(val imageResource: Int, val carName: String, val gasConsum: String,
-                       val ethaConsum: String) {
-}
--- app/src/main/java/com/goblintechs/flex_gasolinaouetanol/models/Car.kt
+++
@@ -1,5 +0,0 @@
-package com.goblintechs.flex_gasolinaouetanol.models
-
-class Car(var nome: String, var gasConsum: String, var ethaConsum: String) {
-
-}
